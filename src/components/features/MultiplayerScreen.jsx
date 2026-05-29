26d895a0b7e680c092db5941fe906c49635970f2
hydrotechvn <mvtctn@gmail.com>
2026-05-29 13:22:52 +0700

    Fix missing Suspense and MultiplayerEngine imports in MultiplayerScreen

--- src/components/features/MultiplayerScreen.jsx
+++ src/components/features/MultiplayerScreen.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { isConnectedToFirebase } from '../../firebase';
+
+const MultiplayerEngine = React.lazy(() => import('../../MultiplayerEngine').then(module => ({ default: module.MultiplayerEngine })));
 
 import playersData from '../../players.json';
 import { 
